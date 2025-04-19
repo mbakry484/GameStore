dcac98dfef9299d7c3e4ba09620a57033d69e64e
Mohamed Bakry <mbakry484@gmail.com>
2025-04-19 22:36:38 +0200

    All Backend Done

--- obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975e60c9cfedba3755bce552877b287d2a03570d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
